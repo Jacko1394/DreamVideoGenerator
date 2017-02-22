bed3c1a4e8f38bb061a784b4764e82d1397ed4c3
Jacko1394 <jacko1394@me.com>
2017-02-22 16:44:48 +1100

    frames to vid

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from os import remove
-
+import os
 from ffmpy import FFmpeg
-
 from PIL import Image
+#import mechanize
 
 myInput = 'IMG_0697.mov'
-myOutput = '/frames/'
+myOutput = os.getcwd() + '/Frames/'
 
-print('hi')
+print('sup')
+'''
+if not os.path.exists(myOutput):
+    os.makedirs(myOutput)
 
 ff = FFmpeg(
     inputs={myInput: None},
-    outputs={None: ["frame%04d.bmp"]}
+    outputs={None: [myOutput + "frame%04d.bmp"]}
 )
 
 ff.run()
 del ff
 
-im = Image.open('frame0400.bmp')
-im.save('frame2.jpg', quality=95)
+for i in range(1, 9999):
+
+    frame = myOutput + 'frame' + format(i, '04d')
+
+    if os.path.isfile(frame + '.bmp'):
+        # print(myOutput + 'frame' + format(i, '04d'))
+        im = Image.open(frame + '.bmp')
+        im.save(frame + '.jpg', quality=95)
+        os.remove(frame + '.bmp')
+    else:
+        break
+'''
+ff = FFmpeg(
+    inputs={myOutput + 'frame%04d.jpg': None},
+    outputs={'test.mp4': ['-r', "60"]}
+)
+
+ff.run()
+del ff
