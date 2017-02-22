0356551e5b615bc14260a6582f80c3abfb161a0b
Jacko1394 <jacko1394@me.com>
2017-02-22 13:15:48 +1100

    PIL stuff

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-# import sys
+from os import remove
 
-import ffmpy
+from ffmpy import FFmpeg
+
+from PIL import Image
+
+myInput = 'IMG_0697.mov'
+myOutput = '/frames/'
 
 print('hi')
 
-ff = ffmpy.FFmpeg(
-    inputs={'input.mp4': None},
-    outputs={'output.avi': None}
+ff = FFmpeg(
+    inputs={myInput: None},
+    outputs={None: ["frame%04d.bmp"]}
 )
 
 ff.run()
+del ff
+
+im = Image.open('frame0400.bmp')
+im.save('frame2.jpg', quality=95)
