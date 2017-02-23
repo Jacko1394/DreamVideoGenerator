514312d4d73b635dea8bb8b8029a932c78e82b66
Jacko1394 <jacko1394@me.com>
2017-02-23 16:39:55 +1100

    webpage automation getting there

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os, sys
 from re import split
 from ffmpy import FFmpeg
 from PIL import Image
-import mechanize
+
 
 name = split('\.', sys.argv[1])
-myOutput = os.getcwd() + '/' + name[0] + ' Frames/'
+myOutput = os.getcwd() + '/' + name[0] + '_Frames/'
 del name
-
-#'''
+print(myOutput)
+'''
 if not os.path.exists(myOutput):
     os.makedirs(myOutput)
 
@@ -30,7 +30,54 @@ for i in range(1, 9999):
         os.remove(frame + '.bmp')
     else:
         break
-#'''
+'''
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import contextlib
+from selenium.webdriver import Remote
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import staleness_of
+
+
+def wait_for_page_load(self, timeout=30):
+    old_page = self.find_element_by_tag_name('html')
+    yield
+    WebDriverWait(self, timeout).until(staleness_of(old_page))
+
+driver = webdriver.Firefox()
+
+driver.implicitly_wait(20)
+
+driver.get('https://deepdreamgenerator.com/generator')
+
+
+elem = driver.find_element_by_name('email')
+elem.clear()
+elem.send_keys('jacko1394@me.com')
+elem = driver.find_element_by_name('password')
+elem.clear()
+elem.send_keys('VFy-LHd-25E-duR')
+elem = driver.find_element_by_class_name('btn')
+elem.click()
+
+# wait_for_page_load(driver, timeout=10)
+# wait_for_page_load(driver, timeout=10)
+
+#WebDriverWait(driver, 15)
+elem = driver.find_element_by_id('image-for-dream')
+#elem.click()
+elem.send_keys('/Users/AllEqualsNothing/Documents/DreamVideoGenerator/IMG_0697_Frames/frame0300.jpg')
+
+#elem.send_keys(Keys.RETURN)
+
+#driver.get('https://deepdreamgenerator.com/generator-style')
+
+#assert "No results found." not in driver.page_source
+driver.close()
+
+'''
 ff = FFmpeg(
     inputs={myOutput + 'frame%04d.jpg': ["-framerate", sys.argv[2]]},
     outputs={'test.mp4': None}
@@ -38,3 +85,4 @@ ff = FFmpeg(
 
 ff.run()
 del ff
+'''
